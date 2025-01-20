c47d826ba2250b1a9236af97a319979b61f5a329
greendou <hezhuning@codemao.cn>
2025-01-20 14:19:45 +0800

    build: test

--- build.rs
+++ build.rs
@@ -110,7 +110,7 @@ fn main() {
     // cargo publish doesn't like pyc files.
     env::set_var("PYTHONDONTWRITEBYTECODE", "1");
 
-    build_v8(is_asan);
+    // build_v8(is_asan);
     build_binding();
 
     return;
@@ -139,6 +139,34 @@ fn acquire_lock() -> LockFile {
 }
 
 fn build_binding() {
+  // NDK 23 and above removes libgcc entirely.
+  // https://github.com/rust-lang/rust/pull/85806
+  env::set_var("CLANG_PATH", "./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang++");
+  env::set_var("LIBCLANG_PATH", "./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/lib");
+  env::set_var("LIBCLANG_STATIC_PATH", "./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/lib");
+  if !Path::new("./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang++").exists() {
+        assert!(Command::new("curl")
+        .arg("-L")
+        .arg("-o").arg("./third_party/android-ndk-r26c-linux.zip")
+        .arg("https://dl.google.com/android/repository/android-ndk-r26c-linux.zip")
+        .status()
+        .unwrap()
+        .success());
+
+        assert!(Command::new("unzip")
+        .arg("-d").arg("./third_party/")
+        .arg("-o")
+        .arg("-q")
+        .arg("./third_party/android-ndk-r26c-linux.zip")
+        .status()
+        .unwrap()
+        .success());
+
+        // new dir if not exist
+        fs::create_dir_all("./third_party/android_toolchain").unwrap();
+        fs::rename("./third_party/android-ndk-r26c", "./third_party/android_toolchain/ndk").unwrap();
+        fs::remove_file("./third_party/android-ndk-r26c-linux.zip").unwrap();
+      }
   let output = Command::new(python())
     .arg("./tools/get_bindgen_args.py")
     .arg("--gn-out")
@@ -151,7 +179,14 @@ fn build_binding() {
   let bindings = bindgen::Builder::default()
     .header("src/binding.hpp")
     .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-    .clang_args(["-x", "c++", "-std=c++20", "-Iv8/include", "-I."])
+    .clang_args([
+      "-x",
+      "c++",
+      "-std=c++20",
+      "--sysroot=./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/sysroot",
+      "-Iv8/include",
+      "-I.",
+    ])
     .clang_args(args)
     .generate_cstr(true)
     .rustified_enum(".*UseCounterFeature")
@@ -273,6 +308,9 @@ fn build_v8(is_asan: bool) {
 
     // NDK 23 and above removes libgcc entirely.
     // https://github.com/rust-lang/rust/pull/85806
+    env::set_var("CLANG_PATH", "./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang++");
+    env::set_var("LIBCLANG_PATH", "./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/lib");
+    env::set_var("LIBCLANG_STATIC_PATH", "./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/lib");
     if !Path::new("./third_party/android_toolchain/ndk/toolchains/llvm/prebuilt/linux-x86_64/bin/aarch64-linux-android26-clang++").exists() {
         assert!(Command::new("curl")
         .arg("-L")
